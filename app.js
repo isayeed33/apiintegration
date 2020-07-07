8aa2d0b7d7de57f0373a0e31eef9c3e5b38fc5be
Irfan <isayeed33@gmail.com>
2020-07-07 21:07:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+
+//MiddleWares
+app.use(cors());
+app.use(bodyParser.json());
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+app.use('/posts', postsRoute);
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('We are on home');
+})
+
+//Connect to DB
+mongoose.connect( process.env.DB_CONNECTION, {useNewUrlParser: true},  () => console.log('Connected to DB'))
+
+//Listen to server
+app.listen(3000);
---
+++ routes/posts.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Posts');
+
+
+//get back all the posts
+router.get('/', async(req, res) => {
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }catch(err){
+        res.json({message: err})
+    }
+});
+
+
+//Submits posts
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+
+    try{
+    const savedPost = await post.save();
+    res.json(savedPost);
+    }catch(err){
+        res.json({message: err})
+    }
+});
+
+
+//Specific posts
+router.get('/:postId', async(req, res) => {
+    try{
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+    }catch{
+        res.json({message: err});
+    }
+});
+
+//Delete Post
+router.delete('/:postId', async(req, res) => {
+    try{
+    const removedPost = await Post.remove({_id: req.params.postId})
+    res.json(removedPost)
+    }catch(err){
+        res.json({message: err});
+    }
+})
+
+//Update Post
+router.patch('/postId', async(req, res) => {
+    try{
+    const updatedPost = await Post.updateOne({_id: req.params.postId}, {$set: {title: req.body.title}});
+    res.json(updatedPost);
+    }catch(err) {
+        res.json({message: err});
+    }
+})
+
+module.exports = router;
